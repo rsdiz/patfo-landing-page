95c63a6319da8764b0f2ab22d46bd52270a2de57
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-09-15 15:58:55 +0700

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,41 @@
 import { useState, useEffect } from "react";
-import { Navigation } from "./components/navigation";
-import { Header } from "./components/header";
-import { Features } from "./components/features";
-import { About } from "./components/about";
-import { Services } from "./components/services";
-import { Gallery } from "./components/gallery";
-import { Testimonials } from "./components/testimonials";
-import { Team } from "./components/Team";
-import { Contact } from "./components/contact";
+import Navigation from "./components/Navigation";
+import Header from "./components/Header";
+import Features from "./components/Features";
+import About from "./components/About";
+import Services from "./components/Services";
+import Gallery from "./components/Gallery";
+import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
+import Client from "./components/Client";
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
   speedAsDuration: true,
 });
 
-const App = () => {
-  const [landingPageData, setLandingPageData] = useState({});
+function App() {
+  const [patfoData, setpatfoData] = useState({});
   useEffect(() => {
-    setLandingPageData(JsonData);
+    setpatfoData(JsonData);
   }, []);
 
   return (
-    <div>
+    <div className="App">
       <Navigation />
-      <Header data={landingPageData.Header} />
-      <Features data={landingPageData.Features} />
-      <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
-      <Gallery data={landingPageData.Gallery}/>
-      <Testimonials data={landingPageData.Testimonials} />
-      <Team data={landingPageData.Team} />
-      <Contact data={landingPageData.Contact} />
+      <Header data={patfoData.Header} />
+      <Features data={patfoData.Features} />
+      <About data={patfoData.About} />
+      <Services data={patfoData.Services} />
+      <Gallery data={patfoData.Gallery} />
+      <Testimonials data={patfoData.Testimonials} />
+      <Client data={patfoData.Client} />
+      <Contact data={patfoData.Contact} />
     </div>
   );
-};
+}
 
 export default App;
